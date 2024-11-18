996ddc487493ae5563d7dd081275ef2c776c1b73
Vlad <123kkrvlad@gmail.com>
2024-11-19 00:51:08 +0200

    add DB interface + test`s

---
+++ internal/repository/test/bot_repo_test.go
@@ -0,0 +1,117 @@
+package test
+
+import (
+	"context"
+	"errors"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/vladislavprovich/TG-bot/internal/repository"
+	mocks "github.com/vladislavprovich/TG-bot/mocks"
+	"go.uber.org/mock/gomock"
+	"testing"
+)
+
+func TestSaveURL(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mocks.NewMockDBExecutor(ctrl)
+
+	mockDB.EXPECT().ExecContext(gomock.Any(), "INSERT INTO urls (user_id, original_url, short_url) VALUES (?, ?, ?)",
+		"user123", "http://example.com", "http://short.url").Return(nil, nil)
+	logger := logrus.New()
+	repo := repository.NewBotRepository(mockDB, *logger)
+	err := repo.SaveURL(context.TODO(), repository.SaveUrlRequest{
+		UserID: "user123",
+		URL: &repository.URLCombined{
+			OriginalURL: "http://example.com",
+			ShortURL:    "http://short.url",
+		},
+	})
+	require.NoError(t, err)
+}
+
+func TestGetListURL(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mocks.NewMockDBExecutor(ctrl)
+
+	mockDB.EXPECT().QueryContext(gomock.Any(), "SELECT original_url, short_url FROM urls WHERE user_id = ?", "user123").
+		Return([]*repository.URLCombined{
+			{OriginalURL: "http://example.com", ShortURL: "http://short.url"},
+		}, nil)
+
+	logger := logrus.New()
+	repo := repository.NewBotRepository(mockDB, *logger)
+
+	_, err := repo.GetListURL(context.TODO(), "user123")
+
+	require.NoError(t, err)
+}
+
+func TestDeleteAllURL(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mocks.NewMockDBExecutor(ctrl)
+
+	mockDB.EXPECT().ExecContext(gomock.Any(), "DELETE FROM urls WHERE user_id = ?", "user123").Return(nil, nil)
+
+	logger := logrus.New()
+
+	repo := repository.NewBotRepository(mockDB, *logger)
+
+	err := repo.DeleteAllURL(context.TODO(), "user123")
+
+	require.NoError(t, err)
+}
+
+func TestDeleteURL(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockDB := mocks.NewMockDBExecutor(ctrl)
+
+	mockDB.EXPECT().ExecContext(gomock.Any(), "DELETE FROM urls WHERE user_id = ? AND original_url = ?", "user123", "http://example.com").Return(nil, nil)
+
+	logger := logrus.New()
+
+	repo := repository.NewBotRepository(mockDB, *logger)
+
+	err := repo.DeleteURL(context.TODO(), "user123", "http://example.com")
+
+	require.NoError(t, err)
+}
+
+func TestDeleteAllUrl_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mocks.NewMockDBExecutor(ctrl)
+
+	mockDB.EXPECT().ExecContext(gomock.Any(), "DELETE FROM urls WHERE user_id = ?", "user123").Return(nil, errors.New("database error"))
+
+	logger := logrus.New()
+
+	repo := repository.NewBotRepository(mockDB, *logger)
+
+	err := repo.DeleteAllURL(context.TODO(), "user123")
+
+	require.Error(t, err)
+}
+
+func TestDeleteURL_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockDB := mocks.NewMockDBExecutor(ctrl)
+
+	mockDB.EXPECT().ExecContext(gomock.Any(), "DELETE FROM urls WHERE user_id = ? AND original_url = ?", "user123", "http://example.com").Return(nil, errors.New("database error"))
+
+	logger := logrus.New()
+
+	repo := repository.NewBotRepository(mockDB, *logger)
+
+	err := repo.DeleteURL(context.TODO(), "user123", "http://example.com")
+
+	require.Error(t, err)
+}
